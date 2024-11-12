72a4f930f1cb3d321358fc63b238964322815850
mikelyferreiradossantos <147520312+mikelyferreiradossantos@users.noreply.github.com>
2024-11-12 10:39:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é IA?',
+    'A inteligência artificial é um campo de estudo multidisciplinar que abrange varias áreas do conhecimento'
+)
+
+criaCartao(
+    'Programação',
+    'Quais são as linguagens mais populares do POO?',
+    'C++, Python, PHP, Java, Javascript'
+)
+
+criaCartao(
+    'Programação',
+    'O que é substituição de métodos?',
+    'Na programação orientada a objetos, a substituição de método permite que a subclasse/classe filha redefina os métodos dentro da classe pai/base'
+)
+
+criaCartao(
+    'Programação',
+    'o que é E-mail?',
+    'Correio eletrônico, mais conhecido como e-mail, é um método de comunicação que usa dispositivos eletrônicos para entregar mensagens em redes de computadores'
+)
+
+criaCartao(
+    'Programação',
+    'Como é gerada a Internet?',
+    'O hardware é como se fossem os cabos, sim esses de fibra óptica, que interligam todo o globo terrestre por debaixo do mar, esses cabos formam o backbone, a espinha dorsal da internet, ele se divide entre os continentes, construindo assim uma rede global de conexões'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é a maior Hidrelétrica do mundo?',
+    'Usina de Três Gargantas – China. A usina de Três Gargantas produz 22.500 MW de energia.'
+)
